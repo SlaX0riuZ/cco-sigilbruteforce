e09d117203325e4ddd1ae61941ad466357736dc1
SlaX0riuZ <slaxoriuz@gmail.com>
2025-02-26 11:40:10 -0600

    base commit, add a current TODO list

---
+++ main.py
@@ -0,0 +1,12 @@
+sdict = {
+    "brim": 0.7, # brimstone T7, increases price by 70%
+    "dice": 7, # dice T7, increases tally price modifier by +7
+    "eventhorizon": 2.1, # event horizon T7, increases tally cap by 210%
+    "neon": 7, # neon T7, increases materials on cube by 7 per material
+    "boom": 21, # boom T7, increases prefix multiplier by 21/prefix
+    "pencil": 2.1, # pencil T7, increases tour rewards by 210%
+    "pie": 2.1, # pie T7, increases price by 210% if tallies are maxed (assumed)
+    "fortune": .21, # fortune T7, increases price by 21% per material on cube
+    "wood": 7, # wood T7, increases prefix multiplier by 7 per 35k tallies, max 28x
+    "grape": 175000 # grape T7, adds 175000 full-powered tallies to cube
+}
