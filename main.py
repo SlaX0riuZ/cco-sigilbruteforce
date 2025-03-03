3a44b7b11569c03fb10f904808eee73a703567ac
SlaX0riuZ <slaxoriuz@gmail.com>
2025-03-03 11:16:59 -0600

    create cube as class, test print + dictionary keep

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-sdict = {
-    "brim": 0.7, # brimstone T7, increases price by 70%
-    "dice": 7, # dice T7, increases tally price modifier by +7
-    "eventhorizon": 2.1, # event horizon T7, increases tally cap by 210%
-    "neon": 7, # neon T7, increases materials on cube by 7 per material
-    "boom": 21, # boom T7, increases prefix multiplier by 21/prefix
-    "pencil": 2.1, # pencil T7, increases tour rewards by 210%
-    "pie": 2.1, # pie T7, increases price by 210% if tallies are maxed (assumed)
-    "fortune": .21, # fortune T7, increases price by 21% per material on cube
-    "wood": 7, # wood T7, increases prefix multiplier by 7 per 35k tallies, max 28x
-    "grape": 175000 # grape T7, adds 175000 full-powered tallies to cube
-}
+import dictkeep
+
+class createCube:
+    def __init__(self, initialprice, tallycap, prefixarray, rarity, sigilslots): # assume cube ALWAYS has maximum tallies
+        self.initialprice = initialprice
+        self.tallycap = tallycap
+        self.prefixarray = prefixarray
+        self.rarity = rarity
+        self.sigilslots = sigilslots
+        self.sigilarray=[]
+        for i in range(sigilslots):
+            self.sigilarray.append('empty')
+    
+    def __str__(self):
+        return f'Base Price: {self.initialprice}, Tally Cap: {self.tallycap}, Prefix Array: {self.prefixarray}\nRarity: {self.rarity}, Sigil Slots: {self.sigilarray}'
+    
+    
+
+testcube1 = createCube(65000, 15000, [45], "relic", 5) # example of tallying relic cube, 1x prefixes
+testcube2 = createCube(139222, -1, [79, 13], "cubic", 5) # example of cubic cube, 2x prefixes
+testcube3 = createCube(1290284, 250000, [-1], "gold", 5) # example of tallying gold cube, 0x prefixes
+
+print(testcube1)
