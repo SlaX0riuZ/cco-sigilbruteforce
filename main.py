8cd3313af4cc77a52997b2d571b87d4d58f008ab
SlaX0riuZ <slaxoriuz@gmail.com>
2025-03-04 11:25:55 -0600

    redoing base object

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import dictkeep
 
-class createCube:
-    def __init__(self, initialprice, tallycap, prefixarray, sigilslots, divbaseincrease, divpricemultiplier, coldivpricemultiplier, coldivpriceaddition): # assume cube ALWAYS has maximum tallies
-        # Initialize variables that are force-filled on creation
-        self.initialprice = initialprice
+# Required Variables:
+'''Base (Non-Col):
+
+-> Base Price
+-> Tally Cap (set to -1 if it doesn't exist)
+-> Prefix Array (set to [-1] if there aren't prefixes)
+-> B-Side Boolean to mark if cube is b-side (use True/False)
+-> Number of base materials
+-> Number of sigil slots
+-> Cube's rarity
+
+'''
+
+class createBaseCube():
+    def __init__(self, baseprice, tallycap, prefixarray, materialcount, sigilslots, rarity, bsidebool):
+        self.baseprice = baseprice
         self.tallycap = tallycap
         self.prefixarray = prefixarray
+        self.materialcount = materialcount
         self.sigilslots = sigilslots
-        self.divbaseincrease = divbaseincrease
-        self.divpricemultiplier = divpricemultiplier
-        self.coldivpricemultiplier = coldivpricemultiplier
-        self.coldivpriceaddition = coldivpriceaddition
-        # Initialize cube's base sigil array
-        self.sigilarray=[]
-        for i in range(sigilslots):
-            self.sigilarray.append('empty')
-    
-    def __str__(self):
-        pass # fill later
+        self.rarity = rarity
+        self.bsidebool = bsidebool
+        
+        if self.bsidebool:
+            self.tallymultiplier = 24
+        else:
+            self.tallymultiplier = 6
